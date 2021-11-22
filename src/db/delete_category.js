32264ddadf61be1d398d95701f9b4647685db8aa
vitaliyhtml5 <vitaliyhtml5@gmail.com>
2021-11-22 19:33:04 +0200

    fix bug about delete category

--- src/db/delete_category.js
+++ src/db/delete_category.js
@@ -1,7 +1,11 @@
 const connection = require('./dbc.js');
 
 const deleteCategory = (category_id, callback) => {
-    const q = `DELETE FROM category WHERE id = ${category_id};`;
+    const q = `
+    DELETE category, words FROM category
+    INNER JOIN words
+    ON words.category_id = category.id
+    WHERE words.category_id = ${category_id} OR category.id = ${category_id}`;
 
     connection.query(q, (err, results) => {
         if (err) {
